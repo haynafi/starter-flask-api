3ef1bab41212171be68470b83e67f8e91841d7d9
haynafi <myorigami13@gmail.com>
2023-10-18 23:41:12 +0700

    testing boom

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, jsonify, request
-# from flask_restful import Resource, Api 
 import os
 
 app = Flask(__name__)
@@ -9,6 +8,10 @@ def hello_world():
     return 'Hello, world!'
 
 @app.route('/boom')
-def hello_world():
+def boom():
     return 'duar!'
 
+# driver function 
+if __name__ == '__main__': 
+  
+    app.run(debug = True) 
