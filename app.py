7a51b7753dc24223199fd976f010e9b5cfd27a4b
haynafi <myorigami13@gmail.com>
2023-10-18 23:37:12 +0700

    testing other output

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+# from flask_restful import Resource, Api 
 import os
 
 app = Flask(__name__)
@@ -6,3 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, world!'
+
+@app.route('/boom')
+def hello_world():
+    return 'duar!'
+
+# driver function 
+if __name__ == '__main__': 
+  
+    app.run(debug = True) 
